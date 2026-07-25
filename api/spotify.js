1180d805201fa3c47e5ffb70010d5a834a74a5cb
playboifarti01 <dankestmemester123@gmail.com>
2026-07-25 00:05:17 -0700

    reinput api

--- api/spotify.js
+++ api/spotify.js
@@ -83,8 +83,8 @@ export default async function handler(req, res) {
 
 }
 
-export default function handler(req, res) {
-  res.status(200).json({
-    message: "API works",
-  });
-}
+// export default function handler(req, res) {
+//   res.status(200).json({
+//     message: "API works",
+//   });
+// }
