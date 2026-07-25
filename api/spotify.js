1259fed05545a27258e0aba24b5955eb0b784df7
playboifarti01 <dankestmemester123@gmail.com>
2026-07-24 23:59:51 -0700

    redo

--- api/spotify.js
+++ api/spotify.js
@@ -1,82 +1,87 @@
-// import axios from "axios";
+import axios from "axios";
 
-// export default async function handler(req, res) {
+export default async function handler(req, res) {
 
-//   try {
+  try {
 
-//     const tokenResponse = await axios.post(
-//       "https://accounts.spotify.com/api/token",
+    const tokenResponse = await axios.post(
+      "https://accounts.spotify.com/api/token",
 
-//       new URLSearchParams({
-//         grant_type: "refresh_token",
-//         refresh_token: process.env.SPOTIFY_REFRESH_TOKEN
-//       }),
+      new URLSearchParams({
+        grant_type: "refresh_token",
+        refresh_token: process.env.SPOTIFY_REFRESH_TOKEN
+      }),
 
-//       {
-//         headers: {
-//           "Content-Type":
-//           "application/x-www-form-urlencoded",
+      {
+        headers: {
+          "Content-Type":
+          "application/x-www-form-urlencoded",
 
-//           Authorization:
-//           "Basic " +
-//           Buffer.from(
-//             process.env.SPOTIFY_CLIENT_ID +
-//             ":" +
-//             process.env.SPOTIFY_CLIENT_SECRET
-//           ).toString("base64")
-//         }
-//       }
-//     );
+          Authorization:
+          "Basic " +
+          Buffer.from(
+            process.env.SPOTIFY_CLIENT_ID +
+            ":" +
+            process.env.SPOTIFY_CLIENT_SECRET
+          ).toString("base64")
+        }
+      }
+    );
 
-//     const accessToken =
-//       tokenResponse.data.access_token;
 
-//     const current =
-//       await axios.get(
-//         "https://api.spotify.com/v1/me/player",
-//         {
-//           headers: {
-//             Authorization:
-//             `Bearer ${accessToken}`
-//           }
-//         }
-//       )
-//       .catch(() => ({
-//         data:null
-//       }));
+    const accessToken =
+      tokenResponse.data.access_token;
 
-//     const history =
-//       await axios.get(
-//         "https://api.spotify.com/v1/me/player/recently-played",
-//         {
-//           params:{
-//             limit:20
-//           },
-//           headers:{
-//             Authorization:
-//             `Bearer ${accessToken}`
-//           }
-//         }
-//       );
 
-//     res.status(200).json({
-//       current: current.data,
-//       history: history.data.items
-//     });
+    const current =
+      await axios.get(
+        "https://api.spotify.com/v1/me/player",
+        {
+          headers: {
+            Authorization:
+            `Bearer ${accessToken}`
+          }
+        }
+      )
+      .catch(() => ({
+        data:null
+      }));
 
-//   } catch(error) {
 
-//     console.log(error.response?.data || error.message);
+    const history =
+      await axios.get(
+        "https://api.spotify.com/v1/me/player/recently-played",
+        {
+          params:{
+            limit:20
+          },
+          headers:{
+            Authorization:
+            `Bearer ${accessToken}`
+          }
+        }
+      );
 
-//     res.status(500).json({
-//       error:
-//       error.response?.data ||
-//       error.message
-//     });
 
-//   }
+    res.status(200).json({
+      current: current.data,
+      history: history.data.items
+    });
 
-// }
+
+  } catch(error) {
+
+    console.log(error.response?.data || error.message);
+
+    res.status(500).json({
+      error:
+      error.response?.data ||
+      error.message
+    });
+
+  }
+
+}
 
 export default function handler(req, res) {
   res.status(200).json({
