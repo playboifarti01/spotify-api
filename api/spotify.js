781f66ad5ffd73f28706926016fdfc305e041c28
playboifarti01 <dankestmemester123@gmail.com>
2026-07-24 23:55:30 -0700

    test api

--- api/spotify.js
+++ api/spotify.js
@@ -1,66 +1,81 @@
 // import axios from "axios";
 
 // export default async function handler(req, res) {
-//   // Get fresh access token
 
-//   const tokenResponse = await axios.post(
-//     "https://accounts.spotify.com/api/token",
+//   try {
 
-//     new URLSearchParams({
-//       grant_type: "refresh_token",
-//       refresh_token: process.env.BQBgaXtk4Rfq04zUgXqJ5cN-v41DFM8VgyJV42bieCdebGPgyB-YEuBNDZ6LSZKi2vCsc41nLKLiwmmATp5JLBDmXJ46xr_z44Ul54XSRsZG4d6_O06IWxcaqfrX2h84yI-Kome0-jda_PORYsEdyck28UIpA8aef3qQSRhw9l8QtTcTlsETyUgM4YQqDoOKGePKAl1pcN-UNMpOQEYoC6k-ZohwdBT8nYrswt8R8MrnWFmELQlr_PrNjyGNrdCF,
-//     }),
+//     const tokenResponse = await axios.post(
+//       "https://accounts.spotify.com/api/token",
 
-//     {
-//       headers: {
-//         "Content-Type": "application/x-www-form-urlencoded",
+//       new URLSearchParams({
+//         grant_type: "refresh_token",
+//         refresh_token: process.env.SPOTIFY_REFRESH_TOKEN
+//       }),
 
-//         Authorization:
+//       {
+//         headers: {
+//           "Content-Type":
+//           "application/x-www-form-urlencoded",
+
+//           Authorization:
 //           "Basic " +
 //           Buffer.from(
-//             process.env.c131f445990c471b9d91063c1d547349 +
-//               ":" +
-//               process.env.f475361d337e4c23967d573b3e1e0e88,
-//           ).toString("base64"),
-//       },
-//     },
-//   );
+//             process.env.SPOTIFY_CLIENT_ID +
+//             ":" +
+//             process.env.SPOTIFY_CLIENT_SECRET
+//           ).toString("base64")
+//         }
+//       }
+//     );
 
-//   const accessToken = tokenResponse.data.access_token;
+//     const accessToken =
+//       tokenResponse.data.access_token;
 
-//   const headers = {
-//     Authorization: `Bearer ${accessToken}`,
-//   };
+//     const current =
+//       await axios.get(
+//         "https://api.spotify.com/v1/me/player",
+//         {
+//           headers: {
+//             Authorization:
+//             `Bearer ${accessToken}`
+//           }
+//         }
+//       )
+//       .catch(() => ({
+//         data:null
+//       }));
 
-//   // Current song
+//     const history =
+//       await axios.get(
+//         "https://api.spotify.com/v1/me/player/recently-played",
+//         {
+//           params:{
+//             limit:20
+//           },
+//           headers:{
+//             Authorization:
+//             `Bearer ${accessToken}`
+//           }
+//         }
+//       );
 
-//   const current = await axios
-//     .get("https://api.spotify.com/v1/me/player", {
-//       headers,
-//     })
-//     .catch(() => {
-//       return {
-//         data: null,
-//       };
+//     res.status(200).json({
+//       current: current.data,
+//       history: history.data.items
 //     });
 
-//   // Last 20 songs
+//   } catch(error) {
 
-//   const recentlyPlayed = await axios.get(
-//     "https://api.spotify.com/v1/me/player/recently-played",
-//     {
-//       params: {
-//         limit: 20,
-//       },
-//       headers,
-//     },
-//   );
+//     console.log(error.response?.data || error.message);
 
-//   res.json({
-//     current: current.data,
+//     res.status(500).json({
+//       error:
+//       error.response?.data ||
+//       error.message
+//     });
 
-//     history: recentlyPlayed.data.items,
-//   });
+//   }
+
 // }
 
 export default function handler(req, res) {
